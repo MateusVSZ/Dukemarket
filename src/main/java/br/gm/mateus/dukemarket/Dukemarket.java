9b27d08b1bab57b052105efea2c3a84ad42dc103
MateusVsZ <mateusvieirasabino044@gmail.com>
2026-01-26 11:31:27 -0300

    Inicialização do Projeto

---
+++ src/main/java/br/gm/mateus/dukemarket/Dukemarket.java
@@ -0,0 +1,14 @@
+
+
+package br.gm.mateus.dukemarket;
+
+/**
+ *
+ * @author DIGM1A
+ */
+public class Dukemarket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
